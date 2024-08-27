bf0ee3701d5133ff443d22890180702234c8c927
Kshitiz Boral <kb7989145@gmail.com>
2024-08-27 12:34:14 +0530

    first commit

---
+++ lib/quiz.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+import 'package:quiz/start_screen.dart';
+import 'package:quiz/questions_screen.dart';
+import 'package:quiz/data/questions.dart';
+import 'package:quiz/results_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  List<String> _selectedAnswers = [];
+  var _activeScreen = 'start-screen';
+
+  void _switchScreen() {
+    setState(() {
+      _activeScreen = 'questions-screen';
+    });
+  }
+
+  void _chooseAnswer(String answer) {
+    _selectedAnswers.add(answer);
+
+    if (_selectedAnswers.length == questions.length) {
+      setState(() {
+        _activeScreen = 'results-screen';
+      });
+    }
+  }
+
+  void restartQuiz() {
+    setState(() {
+      _selectedAnswers = [];
+      _activeScreen = 'questions-screen';
+    });
+  }
+
+  @override
+  Widget build(context) {
+    Widget screenWidget = StartScreen(_switchScreen);
+
+    if (_activeScreen == 'questions-screen') {
+      screenWidget = QuestionsScreen(
+        onSelectAnswer: _chooseAnswer,
+      );
+    }
+
+    if (_activeScreen == 'results-screen') {
+      screenWidget = ResultsScreen(
+        chosenAnswers: _selectedAnswers,
+        onRestart: restartQuiz,
+      );
+    }
+
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 32, 118, 204),
+                Color.fromARGB(255, 21, 126, 191),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: screenWidget,
+        ),
+      ),
+    );
+  }
+}
